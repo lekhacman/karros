dd1d2dde02b1d556e0a8b50dff7ee984954b05de
Le Khac Man <mle29@csc.com>
2017-11-14 17:41:48 +0700

    Fix CORS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+"use strict";
 var express = require('express');
 var router = express.Router();
+const request = require("request");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
+});
+router.get("/search", (req, res, next) => {
+    let url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyB-auXZoXF0zQGLgfhMBoJVc_jFywT35hk&input=" + req.param("place");
+    request(url, (err, response, body) => {
+        // console.log(body);
+        res.json(JSON.parse(body));
+    });
 });
 
 module.exports = router;
