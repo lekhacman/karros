be1e8fcaea60fbae983d5bd3223494ac8f24ad83
Khac Man <khacmanle@gmail.com>
2017-11-15 08:38:47 +0700

    [Integration] Yelp autocomplete controller

--- routers/index.router.js
+++ routers/index.router.js
@@ -2,11 +2,13 @@
 const express = require('express');
 let router = express.Router();
 const googleController = require("../controllers/external/google.controller");
+const yelpController = require("../controllers/external/yelp.controller");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
 });
 router.get("/location", googleController.locationSearch);
+router.get("/yelp/autocomplete", yelpController.autocomplete);
 
 module.exports = router;
